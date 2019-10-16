f5f88fea1ffa671fec5eebafb410fc17f4ba4e4b
misocho <misochobrian@gmail.com>
2019-10-16 10:58:22 +0300

    Refactor - Create todo list - Create new todo - Toggle complete

--- src/store.js
+++ src/store.js
@@ -1,10 +1,10 @@
-import { Container } from 'unstated'
+import { Container } from "unstated";
 
 const defaultState = {
   list: [
     {
       id: 1,
-      title: 'First todo',
+      title: "First todo",
       display: false,
       filterCompleted: false,
       filterAll: true,
@@ -13,82 +13,82 @@ const defaultState = {
         {
           id: 1,
           completed: false,
-          text: 'Read README'
+          text: "Read README"
         },
         {
           id: 2,
           completed: false,
-          text: 'Add one todo'
+          text: "Add one todo"
         },
         {
           id: 3,
           completed: false,
-          text: 'Add filters'
+          text: "Add filters"
         },
         {
           id: 4,
           completed: false,
-          text: 'Add multiple lists'
+          text: "Add multiple lists"
         },
         {
           id: 5,
           completed: false,
-          text: 'Optional: add tests'
+          text: "Optional: add tests"
         }
       ]
     },
     {
       id: 2,
-      title: 'Second todo',
+      title: "Second todo",
       display: false,
       todoItems: [
         {
           id: 6,
           completed: false,
-          text: 'Create github repo'
+          text: "Create github repo"
         },
         {
           id: 7,
           completed: false,
-          text: 'Go though source code'
+          text: "Go though source code"
         },
         {
           id: 8,
           completed: false,
-          text: 'Add list todos'
+          text: "Add list todos"
         },
         {
           id: 9,
           completed: false,
-          text: 'Add filter todos'
+          text: "Add filter todos"
         }
       ]
     }
   ]
-}
+};
 
 class TodosContainer extends Container {
   constructor(props) {
-    super(props)
+    super(props);
 
-    this.state = this.readStorage()
+    this.state = this.readStorage();
   }
 
   readStorage() {
     if (window && window.localStorage) {
-      const state = window.localStorage.getItem('appState')
+      const state = window.localStorage.getItem("appState");
       if (state) {
-        return JSON.parse(state)
+        return JSON.parse(state);
       }
     }
 
-    return defaultState
+    return defaultState;
   }
 
   syncStorage() {
     if (window && window.localStorage) {
-      const state = JSON.stringify(this.state)
-      window.localStorage.setItem('appState', state)
+      const state = JSON.stringify(this.state);
+      window.localStorage.setItem("appState", state);
     }
   }
 
@@ -101,67 +101,57 @@ class TodosContainer extends Container {
         return {
           ...todo,
           display
-        }
+        };
       });
 
-      return { list }
+      return { list };
     });
     this.syncStorage();
-  }
+  };
 
   listTodos() {
     return this.state.list;
   }
 
-
   toggleComplete = async (todo, id) => {
-    const currentTodo = this.state.list.find(i => i.id === todo);
-    const item = currentTodo.todoItems.find(i => i.id === id)
-    const completed = !item.completed
-
     // We're using await on setState here because this comes from unstated package, not React
     // See: https://github.com/jamiebuilds/unstated#introducing-unstated
-    await this.setState(state => {
-      const list = state.list.map(listItem => {
-        const updatedTodoList = listItem.todoItems.map(item => (item.id === id ? { ...item, completed } : { ...item }));
-        return {
-          ...listItem,
-          todoItems: updatedTodoList,
-        }
-      })
-      return {
-        ...state,
-        list,
-      }
-    });
-    this.syncStorage()
-  }
+    await this.setState(({ list }) => ({
+      list: list.map(listItem =>
+        listItem.id === todo
+          ? {
+              ...listItem,
+              todoItems: listItem.todoItems.map(t =>
+                t.id === id ? { ...t, completed: !t.completed } : t
+              )
+            }
+          : listItem
+      )
+    }));
+    this.syncStorage();
+  };
 
   createTodo = async (text, todo) => {
-    const defaultState = JSON.parse(localStorage.getItem('appState'));
-    const getTodo = defaultState.list.find(i => i.id === todo)
+    const defaultState = this.readStorage();
+    const getTodo = defaultState.list.find(({ id }) => id === todo);
     const item = {
       completed: false,
       text,
       id: getTodo.todoItems.length + 1
-    }
-    await this.setState(state => {
-      const list = state.list.map(listItem => {
-        if (listItem.id !== todo) return listItem;
-        const todoItems = listItem.todoItems.concat(item)
-        return {
-          ...listItem,
-          todoItems
-        }
-      });
-      return { list }
+    };
 
-    })
-    this.syncStorage()
-  }
+    await this.setState(state => ({
+      list: state.list.map(listItem => {
+        return listItem.id === todo
+          ? { ...listItem, todoItems: listItem.todoItems.concat(item) }
+          : listItem;
+      })
+    }));
+    this.syncStorage();
+  };
 
   createTodoList = async text => {
-    const defaultState = JSON.parse(localStorage.getItem('appState'));
+    const defaultState = JSON.parse(localStorage.getItem("appState"));
 
     const todoList = {
       id: defaultState.list.length + 1,
@@ -170,95 +160,115 @@ class TodosContainer extends Container {
       filterCompleted: false,
       filterActive: false,
       todoItems: []
-    }
-
-    await this.setState(state => {
-      const list = state.list.concat(todoList);
-      return { list }
-    });
-
-    this.syncStorage()
-  }
+    };
+    await this.setState(state => ({
+      list: state.list.concat(todoList)
+    }));
+    this.syncStorage();
+  };
 
   filterAll = async id => {
-    const defaultState = JSON.parse(localStorage.getItem('appState'));
+    const defaultState = JSON.parse(localStorage.getItem("appState"));
     const defaultTodo = defaultState.list.find(i => i.id === id);
     const currentTodo = this.state.list.find(i => i.id === id);
     const filterAll = !currentTodo.filterAll;
 
-    if (currentTodo.filterCompleted) { currentTodo.filterCompleted = false }
-    if (currentTodo.filterActive) { currentTodo.filterActive = false }
+    if (currentTodo.filterCompleted) {
+      currentTodo.filterCompleted = false;
+    }
+    if (currentTodo.filterActive) {
+      currentTodo.filterActive = false;
+    }
     await this.setState(state => {
       const list = state.list.map(todo => {
         if (todo.id !== id) return todo;
         return {
           ...todo,
           filterAll
-        }
+        };
       });
-      return { list }
+      return { list };
     });
 
-    const todos = filterAll ? defaultTodo.todoItems : defaultTodo.todoItems;
+    const todos = defaultTodo.todoItems;
     this.updateStateByFilter(id, { todos });
-  }
+  };
 
   filterActive = async id => {
-    const defaultState = JSON.parse(localStorage.getItem('appState'));
+    const defaultState = JSON.parse(localStorage.getItem("appState"));
     const defaultTodo = defaultState.list.find(i => i.id === id);
     const currentTodo = this.state.list.find(i => i.id === id);
     const filterActive = !currentTodo.filterActive;
-    if (currentTodo.filterCompleted) { currentTodo.filterCompleted = false }
-    if (currentTodo.filterAll) { currentTodo.filterAll = false }
+    if (currentTodo.filterCompleted) {
+      currentTodo.filterCompleted = false;
+    }
+    if (currentTodo.filterAll) {
+      currentTodo.filterAll = false;
+    }
     await this.setState(state => {
       const list = state.list.map(todo => {
         if (todo.id !== id) return todo;
         return {
           ...todo,
           filterActive
-        }
+        };
       });
-      return { list }
+      return { list };
     });
 
-    const todos = filterActive ? defaultTodo.todoItems.filter(i => i.completed === false) : defaultTodo.todoItems;
+    const todos = filterActive
+      ? defaultTodo.todoItems.filter(i => i.completed === false)
+      : defaultTodo.todoItems;
+    console.log(defaultTodo);
     this.updateStateByFilter(id, { todos });
+  };
+
+  filter = todoId => criteria => {
+    const { list } = this.readStorage()
+    const { todoItems } = list.find(({ id }) => id === todoId)
+    return todoItems.filter(criteria)
   }
 
   filterCompleted = async id => {
-    const defaultState = JSON.parse(localStorage.getItem('appState'));
+    const defaultState = JSON.parse(localStorage.getItem("appState"));
     const defaultTodo = defaultState.list.find(i => i.id === id);
     const currentTodo = this.state.list.find(i => i.id === id);
     const filterCompleted = !currentTodo.filterCompleted;
-    if (currentTodo.filterActive) { currentTodo.filterActive = false }
-    if (currentTodo.filterAll) { currentTodo.filterAll = false }
+    if (currentTodo.filterActive) {
+      currentTodo.filterActive = false;
+    }
+    if (currentTodo.filterAll) {
+      currentTodo.filterAll = false;
+    }
     await this.setState(state => {
       const list = state.list.map(todo => {
         if (todo.id !== id) return todo;
         return {
           ...todo,
           filterCompleted
-        }
+        };
       });
-      return { list }
+      return { list };
     });
 
-    const todos = filterCompleted ? defaultTodo.todoItems.filter(i => i.completed === true) : defaultTodo.todoItems;
+    const todos = filterCompleted
+      ? defaultTodo.todoItems.filter(i => i.completed === true)
+      : defaultTodo.todoItems;
     this.updateStateByFilter(id, { todos });
-  }
+  };
 
   updateStateByFilter = async (id, { todos }) => {
     await this.setState(state => {
       const list = state.list.map(listItem => {
-        if (listItem.id !== id) return { ...listItem }
+        if (listItem.id !== id) return { ...listItem };
         return {
           ...listItem,
           todoItems: todos
-        }
+        };
       });
-      return { list }
+      return { list };
     });
-  }
+  };
 }
 
-export default TodosContainer
+export default TodosContainer;
